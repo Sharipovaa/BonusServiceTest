b5c6c9b49ba8b463a57c2b91b638b20ec1ed1ae7
Sharipovaa <alfya.yanbekova28@gmail.com>
2022-01-31 18:08:28 +0500

    5.1.1.1

--- src/test/java/BonusServiceTest1.java
+++ src/test/java/BonusServiceTest1.java
@@ -2,16 +2,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BonusServiceTest {
-
+class BonusServiceTest1 {
     @Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    void shouldCalculateForNotRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
         long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
+        boolean registered = false;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -21,12 +20,12 @@ public class BonusServiceTest {
     }
 
     @Test
-    void shouldCalculateForRegisteredAndOverLimit() {
+    void shouldCalculateForNotRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
         long amount = 1_000_000_60;
-        boolean registered = true;
+        boolean registered = false;
         long expected = 500;
 
         // вызываем целевой метод:
